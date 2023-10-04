0bbe34903ddda4d2395a778d039a05b3b3d9c09c
Felipe Cunha <felipe_cordeiro12@hotmail.com>
2023-10-04 13:38:07 -0300

    aplicação dos estilos

---
+++ oneBitHealth/src/components/Main/Form/index.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { TextInput, View, Text, Button } from "react-native";
+import ResultImc from "./ResultImc";
+
+
+export default function Form() {
+
+    const [height, setHight] = useState(null)
+    const [weight, setWeight] = useState(null)
+    const [messageImc, setMessageImc] = useState("Preencha o peso e altura")
+    const [imc, setImc] = useState(null)
+    const [textButton, setTextButton] = useState("Calcular")
+
+    function imcCalculator() {
+        return setImc((weight / (height * height)).toFixed(2))
+    }
+
+    function validationImc() {
+        if (weight != null && height != null) {
+            imcCalculator()
+            setHight(null)
+            setWeight(null)
+            setMessageImc("Seu IMC é igual:")
+            setTextButton("Calcular novamente")
+            return
+        }
+        setImc(null)
+        setTextButton("Calcular")
+        setMessageImc("Preencha o peso e altura")
+    }
+
+
+    return (
+        <View>
+            <View>
+                <Text>Altura</Text>
+                <TextInput
+                    onChangeText={setHight}
+                    value={height}
+                    placeholder="Ex. 1.75"
+                    keyboardType="numeric"
+                />
+                <Text>Peso</Text>
+                <TextInput
+                    onChangeText={setWeight}
+                    value={weight}
+                    placeholder="Ex. 75.365"
+                    keyboardType="numeric"
+                />
+                <Button
+                    onPress={() => validationImc()}
+                    title={textButton} />
+            </View>
+            <ResultImc messageResultImc={messageImc} ResultImc={imc} />
+        </View>
+    );
+}
